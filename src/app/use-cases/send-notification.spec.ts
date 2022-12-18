eba02c927a922fff8056c7db98f0e8e9b52e9ec7
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-18 19:24:42 -0300

    feat: create notifications repository

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,7 +1,15 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    console.log(notification);
+  },
+};
+
 describe('Send notification', () => {
   it('should be able to send notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'Example of notification content',
