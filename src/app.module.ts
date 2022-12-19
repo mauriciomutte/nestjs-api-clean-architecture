dfec5e9869e6c5a5342f4c725994d4733aab9b3b
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-19 20:36:05 -0300

    feat: add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from './infra/http/http.module';
+import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule, DatabaseModule],
 })
 export class AppModule {}
