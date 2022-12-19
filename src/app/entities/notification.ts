028030d318de9738bba649fab62a6b7041efce8e
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-18 23:23:40 -0300

    fix: generate id in Notitification entity

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto';
 import { Replace } from 'src/helpers/Replace';
 
 import { Content } from './content';
@@ -11,13 +12,19 @@ interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public get id(): string {
+    return this._id;
   }
 
   public get recipientId(): string {
