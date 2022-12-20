e23bb1af79b0b8b96420c4191c19f9c6cb025e7c
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-20 20:54:48 -0300

    feat: add repository findManyByRecipientId

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -18,6 +18,12 @@ export class inMemoryNotificationsRepository
     return notification;
   }
 
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (notification) => notification.recipientId === recipientId,
+    );
+  }
+
   async countManyByRecipientId(recipientId: string): Promise<number> {
     return this.notifications.filter(
       (notification) => notification.recipientId === recipientId,
