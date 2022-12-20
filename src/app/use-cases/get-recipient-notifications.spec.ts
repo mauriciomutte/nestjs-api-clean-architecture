74d43811d69360292bb48aeddfe9d37702439f5d
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2022-12-20 20:55:19 -0300

    test: get recipient notifications use case

---
+++ src/app/use-cases/get-recipient-notifications.spec.ts
@@ -0,0 +1,37 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { inMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+
+import { GetRecipientNotifications } from './get-recipient-notifications';
+
+describe('Get recipient notifications', () => {
+  it('should be able to get recipient notifications', async () => {
+    const notificationsRepository = new inMemoryNotificationsRepository();
+    const getRecipientNotifications = new GetRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'example-recipient-id' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'example-recipient-id' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'another-recipient-id' }),
+    );
+
+    const { notifications } = await getRecipientNotifications.execute({
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notifications).toHaveLength(2);
+    expect(notifications).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ recipientId: 'example-recipient-id' }),
+        expect.objectContaining({ recipientId: 'example-recipient-id' }),
+      ]),
+    );
+  });
+});
